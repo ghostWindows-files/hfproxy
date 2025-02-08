37dd50316e1a2bcfb168023a119df5b8474da256
ghostWindows-files <156029516+ghostWindows-files@users.noreply.github.com>
2025-02-09 03:01:08 +0800

    Update hproxy.go

--- hproxy.go
+++ hproxy.go
@@ -1,392 +1,155 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
+	"net/url"
 	"os"
-	"regexp"
-	"strings"
-
-	"github.com/joho/godotenv"
+	"path/filepath"
+	"time"
 )
 
-// Config 结构体
-type Config struct {
-	ProxyHostname       string   `json:"PROXY_HOSTNAME"`
-	ProxyProtocol       string   `json:"PROXY_PROTOCOL"`
-	PathnameRegex       string   `json:"PATHNAME_REGEX"`
-	UAWhitelistRegex    string   `json:"UA_WHITELIST_REGEX"`
-	UABlacklistRegex    string   `json:"UA_BLACKLIST_REGEX"`
-	IPWhitelistRegex    string   `json:"IP_WHITELIST_REGEX"`
-	IPBlacklistRegex    string   `json:"IP_BLACKLIST_REGEX"`
-	IPWhitelist         []string `json:"IP_WHITELIST"`
-	IPBlacklist         []string `json:"IP_BLACKLIST"`
-	RegionWhitelistRegex string   `json:"REGION_WHITELIST_REGEX"`
-	RegionBlacklistRegex string   `json:"REGION_BLACKLIST_REGEX"`
-	URL302              string   `json:"URL302"`
-	Debug               bool     `json:"DEBUG"`
-}
-
-// 读取 proxyconfig.json 文件配置
-func loadConfig() Config {
-	var config Config
-
-	file, err := os.Open("proxyconfig.json")
-	if err != nil {
-		log.Println("proxyconfig.json 文件不存在，尝试加载 .env 文件")
-		err = godotenv.Load(".env")
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
-
-		config.ProxyHostname = os.Getenv("PROXY_HOSTNAME")
-		config.ProxyProtocol = os.Getenv("PROXY_PROTOCOL")
-		config.PathnameRegex = os.Getenv("PATHNAME_REGEX")
-		config.UAWhitelistRegex = os.Getenv("UA_WHITELIST_REGEX")
-		config.UABlacklistRegex = os.Getenv("UA_BLACKLIST_REGEX")
-		config.IPWhitelistRegex = os.Getenv("IP_WHITELIST_REGEX")
-		config.IPBlacklistRegex = os.Getenv("IP_BLACKLIST_REGEX")
-		// 从 .env 文件加载 IP_WHITELIST 和 IP_BLACKLIST (JSON 格式)
-		config.IPWhitelist = parseIPListJSON(os.Getenv("IP_WHITELIST"))
-		config.IPBlacklist = parseIPListJSON(os.Getenv("IP_BLACKLIST"))
-		config.RegionWhitelistRegex = os.Getenv("REGION_WHITELIST_REGEX")
-		config.RegionBlacklistRegex = os.Getenv("REGION_BLACKLIST_REGEX")
-		config.URL302 = os.Getenv("URL302")
-		config.Debug = os.Getenv("DEBUG") == "true"
-	} else {
-		defer file.Close()
-
-		// 从 proxyconfig.json 文件中读取配置
-		byteValue, _ := ioutil.ReadAll(file)
-		err = json.Unmarshal(byteValue, &config)
-		if err != nil {
-			log.Fatal("Error parsing proxyconfig.json file:", err)
-		}
-	}
-
-	return config
-}
-
-// 解析 IP 列表字符串 (JSON 格式)
-func parseIPListJSON(ipListStr string) []string {
-	if ipListStr == "" || ipListStr == "[]" || ipListStr == "()" {
-		return []string{} // 返回空切片
-	}
-
-	var ipList []string
-	err := json.Unmarshal([]byte(ipListStr), &ipList)
-	if err != nil {
-		log.Printf("Error parsing IP list: %v", err)
-		return []string{} // 解析失败返回空切片
-	}
-	return ipList
-}
-
-func logError(r *http.Request, message string, clientIP string) {
-	userAgent := r.Header.Get("user-agent")
-	url := r.URL.String()
-	log.Printf("%s, clientIp: %s, user-agent: %s, url: %s", message, clientIP, userAgent, url)
-}
-
-// isFilteredHeader 检查是否是需要过滤的头部
-func isFilteredHeader(key string) bool {
-	lowerKey := strings.ToLower(key)
-	if strings.HasPrefix(lowerKey, "cf-") || strings.HasPrefix(lowerKey, "x-") {
-		return true
-	}
-
-	filteredHeaders := []string{"connection", "origin", "referer", "host", "authority", "link"}
-	for _, h := range filteredHeaders {
-		if lowerKey == h {
-			return true
-		}
-	}
-
-	return false
-}
-
-func createNewRequest(r *http.Request, url, proxyHostname string) (*http.Request, error) {
-	newRequest, err := http.NewRequest(r.Method, url, r.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	// 过滤请求头部
-	filteredHeaders := http.Header{}
-	for key, values := range r.Header {
-		if isFilteredHeader(key) {
-			continue
-		}
-		for _, value := range values {
-			filteredHeaders.Add(key, value)
-		}
-	}
-	newRequest.Header = filteredHeaders
-	//newRequest.Host = proxyHostname // 不需要修改 Host 头部
-
-	return newRequest, nil
-}
-
-func setResponseHeaders(originalResponse *http.Response, proxyHostname, originHostname string, debug bool) http.Header {
-	newResponseHeaders := originalResponse.Header.Clone()
-	// 这一步不需要替换主机名了
-	// for k, v := range newResponseHeaders {
-	// 	for i := range v {
-	// 		newResponseHeaders[k][i] = strings.Replace(v[i], proxyHostname, originHostname, -1)
-	// 	}
-	// }
-	if debug {
-		newResponseHeaders.Del("content-security-policy")
-	}
-	return newResponseHeaders
-}
-
-func replaceResponseText(originalResponse *http.Response, proxyHostname, pathnameRegex, originHostname string) (string, error) {
-	body, err := ioutil.ReadAll(originalResponse.Body)
-	if err != nil {
-		return "", err
-	}
-	text := string(body)
-	if pathnameRegex != "" {
-		pathnameRegex = strings.TrimPrefix(pathnameRegex, "^")
-		re := regexp.MustCompile(`\b` + proxyHostname + `\b(` + pathnameRegex + `)`)
-		text = re.ReplaceAllString(text, originHostname+"$1")
-	} else {
-		re := regexp.MustCompile(`\b` + proxyHostname + `\b`)
-		text = re.ReplaceAllString(text, originHostname)
-	}
-	return text, nil
-}
-
-// 检查 IP 是否在白名单或黑名单中 (支持 IPv6)
-func isIPAllowed(clientIP string, whitelist, blacklist []string) bool {
-	// 如果黑白名单都为空，则允许所有 IP
-	if len(whitelist) == 0 && len(blacklist) == 0 {
-		return true
-	}
-
-	// 将客户端 IP 转换为 net.IP 类型
-	clientIPAddr := net.ParseIP(clientIP)
-	if clientIPAddr == nil {
-		log.Printf("Invalid client IP: %s", clientIP)
-		return false // 如果 IP 地址无效，则拒绝
-	}
-
-	// 检查白名单
-	if len(whitelist) > 0 {
-		for _, ipStr := range whitelist {
-			_, ipNet, err := net.ParseCIDR(ipStr)
-			if err != nil {
-				// 如果不是 CIDR 格式，则尝试解析为单个 IP
-				ip := net.ParseIP(ipStr)
-				if ip == nil {
-					log.Printf("Invalid IP or CIDR in whitelist: %s", ipStr)
-					continue
-				}
-				if clientIPAddr.Equal(ip) {
-					return true
-				}
-			} else {
-				if ipNet.Contains(clientIPAddr) {
-					return true
-				}
-			}
-		}
-	}
-
-	// 检查黑名单
-	if len(blacklist) > 0 {
-		for _, ipStr := range blacklist {
-			_, ipNet, err := net.ParseCIDR(ipStr)
-			if err != nil {
-				// 如果不是 CIDR 格式，则尝试解析为单个 IP
-				ip := net.ParseIP(ipStr)
-				if ip == nil {
-					log.Printf("Invalid IP or CIDR in blacklist: %s", ipStr)
-					continue
-				}
-				if clientIPAddr.Equal(ip) {
-					return false
-				}
-			} else {
-				if ipNet.Contains(clientIPAddr) {
-					return false
-				}
-			}
-		}
-	}
-
-	// 如果存在白名单，并且IP不在白名单中，则拒绝
-	if len(whitelist) > 0 {
-		return false
-	}
-
-	return true // 默认允许
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	config := loadConfig()
-
-	//debug模式下打印所有配置项
-	if config.Debug {
-		log.Println("All configurations:")
-		configJSON, _ := json.MarshalIndent(config, "", "  ")
-		log.Println(string(configJSON))
-	}
-
-	url := r.URL
-	originHostname := url.Hostname()
-	proxyHostname := config.ProxyHostname
-
-	// 获取真实的客户端 IP
-	clientIP, _, err := net.SplitHostPort(r.RemoteAddr)
-	if err != nil {
-		logError(r, "Error getting client IP: "+err.Error(), r.RemoteAddr)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	// 获取请求头中的地区信息
-	clientRegion := r.Header.Get("cf-ipcountry")
-
-	// 请求验证
-	var triggeredRules []string
-
-	if proxyHostname == "" {
-		triggeredRules = append(triggeredRules, "PROXY_HOSTNAME is empty")
-	}
-	if config.PathnameRegex != "" && !regexp.MustCompile(config.PathnameRegex).MatchString(url.Path) {
-		triggeredRules = append(triggeredRules, "PATHNAME_REGEX mismatch: "+config.PathnameRegex)
-	}
-	if config.UAWhitelistRegex != "" && !regexp.MustCompile(config.UAWhitelistRegex).MatchString(strings.ToLower(r.Header.Get("user-agent"))) {
-		triggeredRules = append(triggeredRules, "UA_WHITELIST_REGEX mismatch: "+config.UAWhitelistRegex)
-	}
-	if config.UABlacklistRegex != "" && regexp.MustCompile(config.UABlacklistRegex).MatchString(strings.ToLower(r.Header.Get("user-agent"))) {
-		triggeredRules = append(triggeredRules, "UA_BLACKLIST_REGEX match: "+config.UABlacklistRegex)
-	}
-	if !isIPAllowed(clientIP, config.IPWhitelist, config.IPBlacklist) {
-		if len(config.IPWhitelist) > 0 {
-			triggeredRules = append(triggeredRules, "IP not in IP_WHITELIST")
-		} else {
-			triggeredRules = append(triggeredRules, "IP in IP_BLACKLIST")
-		}
-	}
-	if config.IPWhitelistRegex != "" && !regexp.MustCompile(config.IPWhitelistRegex).MatchString(clientIP) {
-		triggeredRules = append(triggeredRules, "IP_WHITELIST_REGEX mismatch: "+config.IPWhitelistRegex)
-	}
-	if config.IPBlacklistRegex != "" && regexp.MustCompile(config.IPBlacklistRegex).MatchString(clientIP) {
-		triggeredRules = append(triggeredRules, "IP_BLACKLIST_REGEX match: "+config.IPBlacklistRegex)
-	}
-	if clientRegion != "" && config.RegionWhitelistRegex != "" && !regexp.MustCompile(config.RegionWhitelistRegex).MatchString(clientRegion) {
-		triggeredRules = append(triggeredRules, "REGION_WHITELIST_REGEX mismatch: "+config.RegionWhitelistRegex)
-	}
-	if clientRegion != "" && config.RegionBlacklistRegex != "" && regexp.MustCompile(config.RegionBlacklistRegex).MatchString(clientRegion) {
-		triggeredRules = append(triggeredRules, "REGION_BLACKLIST_REGEX match: "+config.RegionBlacklistRegex)
-	}
-
-	if len(triggeredRules) > 0 {
-		logError(r, "Invalid request, triggered rules: "+strings.Join(triggeredRules, ", "), clientIP)
-		if config.URL302 != "" {
-			http.Redirect(w, r, config.URL302, http.StatusFound)
-			return
-		}
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		w.Write([]byte(nginx()))
-		return
-	}
-
-	// 允许访问，输出客户端 IP 和请求 URL
-	userAgent := r.Header.Get("user-agent")
-	log.Printf("Allowed request, clientIp: %s, user-agent: %s, url: %s", clientIP, userAgent, url.String())
-
-	// 设置目标 URL 的主机和协议
-	url.Host = proxyHostname
-	url.Scheme = config.ProxyProtocol
-
-	newRequest, err := createNewRequest(r, url.String(), proxyHostname)
-	if err != nil {
-		logError(r, "Create new request failed", clientIP)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	originalResponse, err := http.DefaultClient.Do(newRequest)
-	if err != nil {
-		logError(r, "Fetch error: "+err.Error(), clientIP)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-	defer originalResponse.Body.Close()
-
-	newResponseHeaders := setResponseHeaders(originalResponse, proxyHostname, originHostname, config.Debug)
-	for k, v := range newResponseHeaders {
-		for _, v2 := range v {
-			w.Header().Add(k, v2)
-		}
-	}
-
-	contentType := newResponseHeaders.Get("content-type")
-	var body string
-	if strings.Contains(contentType, "text/") {
-		body, err = replaceResponseText(originalResponse, proxyHostname, config.PathnameRegex, originHostname)
-		if err != nil {
-			logError(r, "Replace response text error", clientIP)
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-	} else {
-		bodyBytes, err := ioutil.ReadAll(originalResponse.Body)
-		if err != nil {
-			logError(r, "Read original response body error", clientIP)
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-		body = string(bodyBytes)
-	}
-
-	w.WriteHeader(originalResponse.StatusCode)
-	w.Write([]byte(body))
-}
-
-func nginx() string {
-	return `<!DOCTYPE html>
-<html>
-<head>
-<title>Welcome to nginx!</title>
-<style>
-html { color-scheme: light dark; }
-body { width: 35em; margin: 0 auto;
-font-family: Tahoma, Verdana, Arial, sans-serif; }
-</style>
-</head>
-<body>
-<h1>Welcome to nginx!</h1>
-<p>If you see this page, the nginx web server is successfully installed and
-working. Further configuration is required.</p>
-
-<p>For online documentation and support please refer to
-<a href="http://nginx.org/">nginx.org</a>.<br/>
-Commercial support is available at
-<a href="http://nginx.com/">nginx.com</a>.</p>
-
-<p><em>Thank you for using nginx.</em></p>
-</body>
-</html>`
-}
+const (
+	port         = 8089
+	httpDir      = "http" // 存放 HTTP 文件的子目录
+	routeXMLPath = "hide/route.xml"
+)
 
 func main() {
-	// 预先加载配置并打印
-	config := loadConfig()
-	configJSON, _ := json.MarshalIndent(config, "", "  ")
-	fmt.Println("Loaded configurations:")
-	fmt.Println(string(configJSON))
+	// 设置日志同时输出到文件和控制台
+	logFile, err := os.OpenFile("server.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("无法打开日志文件: %v", err)
+	}
+	defer logFile.Close()
+	log.SetOutput(io.MultiWriter(os.Stdout, logFile))
+	log.SetFlags(log.LstdFlags | log.Lshortfile) // 日志中包含时间戳和文件:行号
 
-	http.HandleFunc("/", handler)
-	log.Println("Starting server on :5213")
-	log.Fatal(http.ListenAndServe(":5213", nil))
+	// 如果 http 目录不存在，则创建它
+	if _, err := os.Stat(httpDir); os.IsNotExist(err) {
+		if err := os.Mkdir(httpDir, 0755); err != nil {
+			log.Fatalf("创建 http 目录失败: %v", err)
+		}
+	}
+
+	// HTTP 请求处理函数
+	http.HandleFunc("/", requestHandler)
+
+	// 启动服务器
+	addr := fmt.Sprintf(":%d", port)
+	log.Printf("服务器已启动，端口号 %d", port)
+	log.Printf("针对 /%s 的请求将返回 route.xml", routeXMLPath)
+	err = http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatalf("ListenAndServe 错误: %v", err)
+	}
+}
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+    startTime := time.Now()
+    log.Printf("收到请求: %s %s 来自 %s", r.Method, r.URL, r.RemoteAddr)
+
+    // 检查请求路径是否匹配特定路径
+    if r.URL.Path == "/"+routeXMLPath && r.Method == "GET" {
+        serveRouteXML(w, r)
+    } else {
+        // 处理 POST 请求
+        if r.Method == "POST" {
+            body, err := ioutil.ReadAll(r.Body)
+            if err != nil {
+                log.Printf("读取请求体失败: %v", err)
+                http.Error(w, "读取请求体失败", http.StatusInternalServerError)
+                return
+            }
+            defer r.Body.Close()
+
+            // 解析 POST 数据
+            values, err := url.ParseQuery(string(body))
+            if err != nil {
+                log.Printf("解析 POST 数据失败: %v", err)
+                http.Error(w, "解析 POST 数据失败", http.StatusBadRequest)
+                return
+            }
+
+            // 获取并解码 msg 参数
+            msgEncoded := values.Get("msg")
+            msgDecoded, err := url.QueryUnescape(msgEncoded) // 进行 URL 解码
+            if err != nil {
+                log.Printf("解码 msg 参数失败: %v", err)
+                http.Error(w, "解码 msg 参数失败", http.StatusBadRequest)
+                return
+            }
+            // 输出解码后的 msg 数据
+            log.Printf("收到 POST 请求体数据: msg_id=%s&msg=%s", values.Get("msg_id"), msgDecoded)
+
+            msgID := values.Get("msg_id")
+            if msgID == "" {
+                log.Printf("未找到 msg_id 参数")
+                http.Error(w, "未找到 msg_id 参数", http.StatusBadRequest)
+                return
+            }
+
+            // 根据 msg_id 构建 JSON 文件路径
+            jsonFilePath := filepath.Join(httpDir, msgID+".json")
+
+            // 检查 JSON 文件是否存在
+            if _, err := os.Stat(jsonFilePath); os.IsNotExist(err) {
+                log.Printf("文件 %s 不存在", jsonFilePath)
+                // 返回自定义的错误信息
+                w.Header().Set("Content-Type", "application/json")
+                w.WriteHeader(http.StatusOK) //这里可以改为其他的状态码
+                w.Write([]byte(`{"errorCode":-13,"errorMsg":"`+msgID+`"}`))
+                return
+            }
+
+            // 读取 JSON 文件内容
+            jsonContent, err := ioutil.ReadFile(jsonFilePath)
+            if err != nil {
+                log.Printf("读取文件 %s 失败: %v", jsonFilePath, err)
+                http.Error(w, "服务器内部错误", http.StatusInternalServerError)
+                return
+            }
+
+            // 设置响应头并返回 JSON 内容
+            w.Header().Set("Content-Type", "application/json")
+            w.Write(jsonContent)
+
+            log.Printf("成功返回 %s 给 %s", jsonFilePath, r.RemoteAddr)
+
+        } else {
+            http.NotFound(w, r)
+        }
+    }
+
+    duration := time.Since(startTime)
+    log.Printf("请求处理耗时 %v", duration)
+}
+
+func serveRouteXML(w http.ResponseWriter, r *http.Request) {
+	// 构建 route.xml 的完整路径
+	xmlFilePath := filepath.Join(httpDir, "route.xml")
+
+	// 检查 route.xml 是否存在
+	if _, err := os.Stat(xmlFilePath); os.IsNotExist(err) {
+		log.Printf("route.xml 文件不存在: %s", xmlFilePath)
+		http.NotFound(w, r)
+		return
+	}
+
+	// 读取 route.xml 的内容
+	xmlContent, err := ioutil.ReadFile(xmlFilePath)
+	if err != nil {
+		log.Printf("读取 route.xml 文件失败: %v", err)
+		http.Error(w, "服务器内部错误", http.StatusInternalServerError)
+		return
+	}
+
+	// 设置响应头并发送 XML 内容
+	w.Header().Set("Content-Type", "application/xml")
+	w.Write(xmlContent)
+
+	log.Printf("成功返回 route.xml 给 %s", r.RemoteAddr)
 }
